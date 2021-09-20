032d82745c140c6556bfa526b01dcf01baabbe56
Ali Zakwan <76007907+alizakwan@users.noreply.github.com>
2021-09-20 11:09:35 -0700

    url updated

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-
 var btntranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
-var output=document.querySelector("#output")
+var output = document.querySelector("#output");
 
-console.log(txtinput);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something went wrong try again after some time");
+}
 
 function clickEventHandler() {
-  
+  var inputext = txtinput.value;
+  fetch(getTranslationURL(inputext))
+    .then((response) => {
+      var res = response.json();
+      return res;
+    })
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
 
-    output.innerText= "banabanaban"+txtinput.value
-};
-
-btntranslate.addEventListener("click", clickEventHandler)
+btntranslate.addEventListener("click", clickEventHandler);
